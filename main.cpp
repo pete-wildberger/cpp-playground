49f8ec63c69f7722dea427f4906bd67e78403184
Peter Wildberger <pete.wildberger@gmail.com>
2017-10-07 22:39:24 -0500

    init commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cpplus
+//
+//  Created by Peter on 9/27/17.
+//  Copyright © 2017 Peter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(void) {
+    char Name[21];
+    
+    std::cout << "What is your name?";
+    std::cin >> Name;
+    std::cout << "Good morning " << Name << std::endl;
+
+}
