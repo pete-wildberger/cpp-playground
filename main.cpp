ccd6eed76f64c3a56deec97849bd273874746031
Peter Wildberger <pete.wildberger@gmail.com>
2017-10-08 11:21:04 -0500

    cylinder volume calc

--- main.cpp
+++ main.cpp
@@ -7,15 +7,27 @@
 //
 
 #include <iostream>
+#include <math.h>
 
+//int main(void) {
+//    int pounds;
+//    float kiloCon, kilos;
+//    kiloCon = 2.21;
+//    
+//    std::cout << "How much do you weigh in pounds? ";
+//    std::cin >> pounds;
+//    kilos = pounds / kiloCon;
+//    std::cout << "You weigh " << kilos << " kilos" << std::endl;
+//
+//}
 int main(void) {
-    int pounds;
-    float kiloCon, kilos;
-    kiloCon = 2.21;
-    
-    std::cout << "How much do you weigh in pounds? ";
-    std::cin >> pounds;
-    kilos = pounds / kiloCon;
-    std::cout << "You weigh " << kilos << " kilos" << std::endl;
+    double Volume, Radius, Height;
+    std::cout << "This program tells you the volume of a cylinder. \n";
+    std::cout << "How tall is the tank in inches?\n";
+    std::cin >> Height;
+    std::cout << "What is the radius of the tank in inches?\n";
+    std::cin >> Radius;
+    Volume = (3.1415 * pow(Radius, 2)) * Height;
+    std::cout << "The tank's volume is "<< Volume << "inches cubed!\n";
 
 }
