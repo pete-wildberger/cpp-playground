a9ba631ac12e584f1093a2a37030b6fc099a0ca0
Peter Wildberger <pete.wildberger@gmail.com>
2017-10-09 20:27:53 -0500

    const

--- main.cpp
+++ main.cpp
@@ -9,25 +9,27 @@
 #include <iostream>
 #include <math.h>
 
-//int main(void) {
+//int main() {
 //    int pounds;
 //    float kiloCon, kilos;
 //    kiloCon = 2.21;
 //    
 //    std::cout << "How much do you weigh in pounds? ";
 //    std::cin >> pounds;
-//    kilos = pounds / kiloCon;
+//    kilos = float(pounds) / kiloCon;
 //    std::cout << "You weigh " << kilos << " kilos" << std::endl;
+//    return 0;
 //
 //}
-int main(void) {
+int main() {
+    const float Pi = 3.14159;
     double Volume, Radius, Height;
     std::cout << "This program tells you the volume of a cylinder. \n";
     std::cout << "How tall is the tank in inches?\n";
     std::cin >> Height;
     std::cout << "What is the radius of the tank in inches?\n";
     std::cin >> Radius;
-    Volume = (3.1415 * pow(Radius, 2)) * Height;
+    Volume = (Pi * pow(Radius, 2)) * Height;
     std::cout << "The tank's volume is "<< Volume << "inches cubed!\n";
-
+    return 0;
 }
